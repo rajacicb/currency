6e0ce6e54e876ff4791abff4fadf576acaab924b
rajacicb <rajacicb@gmail.com>
2022-02-06 16:50:34 +0100

    Solution & Shell (MainWindow)

---
+++ CurrencyConverter/CurrencyConverter/App.xaml.cs
@@ -0,0 +1,22 @@
+﻿using CurrencyConverter.Views;
+using Prism.Ioc;
+using Prism.Unity;
+using System.Windows;
+
+namespace CurrencyConverter
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : PrismApplication
+    {
+        protected override Window CreateShell()
+        {
+            return Container.Resolve<MainWindow>();
+        }
+
+        protected override void RegisterTypes(IContainerRegistry containerRegistry)
+        {
+        }
+    }
+}
---
+++ CurrencyConverter/CurrencyConverter/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,18 @@
+﻿using Prism.Mvvm;
+
+namespace CurrencyConverter.ViewModels
+{
+    public class MainWindowViewModel : BindableBase
+    {
+        private string title = "Currency Converter Application";
+        public string Title
+        {
+            get { return title; }
+            set { SetProperty(ref title, value); }
+        }
+
+        public MainWindowViewModel()
+        {
+        }
+    }
+}
